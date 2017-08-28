439214903c1310557a37762f7b6e624b20963a5d
LaughingLeader <laughingleader@gmail.com>
2017-08-28 10:03:25 -0500

    Initial commit

---
+++ Duality/Component/Component.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq; $endif$
+
+using Duality;
+using Duality.Components;
+using Duality.Components.Physics;
+using Duality.Resources;
+using Duality.Editor;
+
+namespace $rootnamespace$
+{
+	public class $safeitemrootname$ : Component, ICmpInitializable
+	{
+		public void OnInit(Component.InitContext context)
+		{
+
+		}
+
+		public void OnShutdown(Component.ShutdownContext context)
+		{
+
+		}
+	}
+}
